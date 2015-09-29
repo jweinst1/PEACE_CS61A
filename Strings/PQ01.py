45b8c1238d230a45eaed8e5cac95f625e5d9a9f0
Joshua Weinstein <jweinst1@berkeley.edu>
2015-09-29 12:26:32 -0700

    string function
    strings

--- Strings/PQ01.py
+++ Strings/PQ01.py
@@ -1,4 +1,6 @@
 __author__ = 'Josh'
 
 def alphabet_sort(*strings):
-
+    """Define a function, alphabet_sort, that takes an arbitrary number of strings and sorts them alphabetically.
+    you can assume all strings are lowercased."""
+    return None
