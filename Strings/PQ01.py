35c4795d447da062b34a43df26459709d80e5959
Joshua Weinstein <jweinst1@berkeley.edu>
2015-09-29 13:01:35 -0700

    new commit
    new commit

--- Strings/PQ01.py
+++ Strings/PQ01.py
@@ -6,5 +6,11 @@ def shortest_len(*strings):
     4"""
     pass
 
+def shortest_str(*strings):
+    #Define a function, shortest_str, that takes an arbitrary number of strings and returns the string with the shortest length.
+    #If two or more strings share the shortest length, it will return a list of those strings.
+    #Hint: consider using your shortest_len function.
+    pass
+
 
 
--- Strings/PQ01Answers.py
+++ Strings/PQ01Answers.py
@@ -13,3 +13,14 @@ def shortest_len(*strings):
         else:
             i += 1
     return len(current)
+
+def shortest_str(*strings):
+    #Define a function, shortest_str, that takes an arbitrary number of strings and returns the string with the shortest length.
+    #If two or more strings share the shortest length, it will return a list of those strings.
+    #Hint: consider using your shortest_len function.
+    shortest = shortest_len(strings)
+    lst = [elem for elem in strings if len(elem) == shortest]
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return lst
