90515d1de7f37a82ff0c29f6aaa7c11fd8caefcb
Joshua Weinstein <jweinst1@berkeley.edu>
2015-09-30 13:29:12 -0700

    added prime functions
    prime functions

--- Lists/PQ01.py
+++ Lists/PQ01.py
@@ -3,3 +3,9 @@ __author__ = 'Josh'
 def merge_all(*lsts):
     #Define a function, merge_all, which takes an arbitrary number of lists an merges them into one.
     pass
+
+def prime_range(n):
+    #Return a list of prime numbers up to n. Try doing this using list comprehensions.
+    #>>> prime_range(50)
+    #[0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    pass
--- Lists/PQ01Answers.py
+++ Lists/PQ01Answers.py
@@ -6,3 +6,12 @@ def merge_all(*lsts):
     for elem in lsts:
         new += elem
     return new
+
+def prime_range(n):
+    #Return a list of prime numbers up to n. Try doing this using list comprehensions.
+    def is_prime(n):
+        for i in range(2, n):
+            if n%i==0:
+                return False
+        return True
+    return [elem for elem in range(n) if is_prime(elem)]
