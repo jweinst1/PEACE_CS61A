078e52572c9615662fe7e641e84652bc2cbde9c7
Joshua Weinstein <jweinst1@berkeley.edu>
2015-09-29 21:06:38 -0700

    several questions added
    string questions

--- Strings/PQ01.py
+++ Strings/PQ01.py
@@ -10,6 +10,23 @@ def shortest_str(*strings):
     #Define a function, shortest_str, that takes an arbitrary number of strings and returns the string with the shortest length.
     #If two or more strings share the shortest length, it will return a list of those strings.
     #Hint: consider using your shortest_len function.
+    #>>> shortest_str('love', 'peace', 'red', 'now')
+    #['red', 'now']
+    pass
+
+def lst_to_str(lst):
+    #Define a function, lst_to_str, that takes a list and joins it together as a new string.
+    #>>> lst_to_str([7, 5, 'a', 4, 5, 3])
+    #'75a453'
+    #>>> f = [x for x in range(10)]
+    #>>> lst_to_str(f)
+    #'0123456789'
+    pass
+
+def repeat_str(char, i):
+    #Define a function, repeat_str, that returns a string containing the char, repeated i times.
+    #>>> repeat_str('b', 50)
+    #'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
     pass
 
 
--- Strings/PQ01Answers.py
+++ Strings/PQ01Answers.py
@@ -18,9 +18,30 @@ def shortest_str(*strings):
     #Define a function, shortest_str, that takes an arbitrary number of strings and returns the string with the shortest length.
     #If two or more strings share the shortest length, it will return a list of those strings.
     #Hint: consider using your shortest_len function.
+    #>>> shortest_str('love', 'peace', 'red', 'now')
+    #['red', 'now']
     shortest = shortest_len(strings)
     lst = [elem for elem in strings if len(elem) == shortest]
     if len(lst) == 1:
         return lst[0]
     else:
         return lst
+
+def lst_to_str(lst):
+    #Define a function, lst_to_str, that takes a list and joins it together as a new string.
+    #>>> lst_to_str([7, 5, 'a', 4, 5, 3])
+    #'75a453'
+    new = ''
+    for elem in lst:
+        new += str(elem)
+    return new
+
+def repeat_str(char, i):
+    #Define a function, repeat_str, that returns a string containing the char, repeated i times.
+    new = ''
+    count = 0
+    while count < i:
+        new += char
+        count += 1
+    return new
+
